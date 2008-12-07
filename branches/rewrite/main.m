0fc661e36e7dce53cadf350962081e2794cbe2f9
kleinweby@kleinweby.de <kleinweby@kleinweby.de@05cbea79-a34c-0410-9d01-bf09dffdfff8>
2008-12-07 09:27:59 +0000

    Add rewrite of iRelayChat...

---
+++ branches/rewrite/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iRelayChat
+//
+//  Created by Christian Speich on 09.11.08.
+//  Copyright __MyCompanyName__ 2008 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
